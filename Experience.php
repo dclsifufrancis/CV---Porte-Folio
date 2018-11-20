94d3fe6d4b8d8887ad6d6a0cc5d7eb7ab596498f
dclsifufrancis <dcl.francisy@18pixel.fr>
2018-11-20 16:56:29 +0100

    mobile1

---
+++ Experience.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+
+    <title>Expériences</title>
+</head>
+<body>
+    <header>
+        <div id="logo">EXPERIENCES</div>
+    </header>
+    <div class="container">
+        <div class="">
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,11 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
 
     <title>CV - Porte-folio</title>
 </head>
 <body>
-    
+    <header>
+        <div id="photo"><img src="Travail préparatoire/Images/Photo.png"></div>
+        <div id="prenomNom" 
+            div class="cadre"><h1>Francis YANG</h1>
+            </div>
+        </div>
+        <div id="poste"><h2>Développeur Web / Web mobile</h2></div>
+    </header>
+    <main>
+        <!-- <div class="container"> -->
+            <div id="haut" class="bloc">
+                <div id="experience" class="picto"><img src="Travail préparatoire/Images/logo-exp2.png"><p>EXPERIENCES</p></div>
+                <div id="formation" class="picto"><img src="Travail préparatoire/Images/logo-formation .png"><p>FORMATIONS</p></div>
+            </div>
+            <div id="bas"class="bloc">
+                <div id="contact"class="picto"><img src="Travail préparatoire/Images/logo-contact.png"><p>CONTACT</p></div>
+                <div id="porteFolio"class="picto"><img src="Travail préparatoire/Images/logo-portefolio.png"><p>PORTE FOLIO</p></div>
+            </div>
+        <!-- </div> -->
+    </main>
+
     <script src="script.js"></script>
 </body>
 </html>
